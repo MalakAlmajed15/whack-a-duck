83b2715153e439a0e5b6fee3b635cc1953170c2a
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-07-09 11:14:39 +0300

    create grid

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
 function init(){
-
+    const gridElm = document.querySelector('.grid')
+    function createGrid(){
+        //for every cell that we require create a div 
+        //append this cell to our grid
+        for(let i=0; i<100; i++){
+            const cell = document.createElement('div')
+            cell.textContent = i//write a number on each sell
+            gridElm.appendChild(cell)
+        }
+    }
+    createGrid()
 }
 
 document.addEventListener('DOMContentLoaded', init)
