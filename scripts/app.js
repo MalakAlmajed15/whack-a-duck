6cd55a6fac54b5064b01a5cf008415875489e3f9
MalakAlmajed15 <malakalmajed15@gmail.com>
2025-07-09 11:40:35 +0300

    add dock class

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 function init(){
     const gridElm = document.querySelector('.grid')
+    const cells = []
+    const gridWidth = 10
+    const numberOfCells = gridWidth * gridWidth
     function createGrid(){
         //for every cell that we require create a div 
         //append this cell to our grid
-        for(let i=0; i<100; i++){
+        for(let i=0; i<numberOfCells; i++){
             const cell = document.createElement('div')
+            cell.classList.add('duck')
             cell.textContent = i//write a number on each sell
+            cells.push(cell)
             gridElm.appendChild(cell)
         }
+        console.log(cells)
     }
     createGrid()
 }
